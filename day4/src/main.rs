3d982754c2401c1a0f1e29e3d4fd9a96fbdbca9b
Justin Barker <jstnbarker@pm.me>
2024-12-04 01:14:51 -0500

    Make solution more generic

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -16,20 +16,19 @@ fn main() {
         for j in 0..characters[0].len(){
             let temp: char = characters[i][j];
             if temp == 'X'{
-                total_xmas += find_xmas(i,j,characters.clone());
+                total_xmas += find_xword(i,j,characters.clone(),"XMAS".to_string());
             }
         }
     }
     println!("{}", total_xmas);
 }
 
-fn find_xmas(i:usize, j: usize, characters: Vec<Vec<char>>) -> i32{
+fn find_xword(i:usize, j: usize, characters: Vec<Vec<char>>, pattern: String) -> i32{
     let offsets: [[i32; 2]; 8] = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
-    let pattern = "XMAS";
     let mut sum: i32 = 0;
     for offset in offsets{
         let mut pos = [i,j];
-        let mut temp: String = "X".to_string();
+        let mut temp: String = characters[pos[0]][pos[1]].to_string();
         while pattern.contains(&temp) {
             // apply offset
             let ni = pos[0] as i32 + offset[0];
