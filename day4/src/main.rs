976fc3cee9f67ccc936475f9de16ae9c8a0751b2
Justin Barker <jstnbarker@pm.me>
2024-12-04 01:11:28 -0500

    Finish part 1; spent a lot of time fighting adding a negative to a usize. Rust doesn't have implicit casting so i jus explicitly did everything. better that way I reckon.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -11,12 +11,51 @@ fn main() {
             characters.push(line.chars().collect());
         }
     }
+    let mut total_xmas: i32 = 0;
     for i in 0..characters.len(){
         for j in 0..characters[0].len(){
             let temp: char = characters[i][j];
             if temp == 'X'{
-                println!("{}", temp);
+                total_xmas += find_xmas(i,j,characters.clone());
             }
         }
     }
+    println!("{}", total_xmas);
+}
+
+fn find_xmas(i:usize, j: usize, characters: Vec<Vec<char>>) -> i32{
+    let offsets: [[i32; 2]; 8] = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
+    let pattern = "XMAS";
+    let mut sum: i32 = 0;
+    for offset in offsets{
+        let mut pos = [i,j];
+        let mut temp: String = "X".to_string();
+        while pattern.contains(&temp) {
+            // apply offset
+            let ni = pos[0] as i32 + offset[0];
+            let nj = pos[1] as i32 + offset[1];
+
+            // make sure applied offset is within boundaries
+            if ni >= 0 && ni < characters.len() as i32 {
+                pos[0] = ni as usize;
+            } else {
+                break;
+            }
+            if nj >= 0 && nj < characters[0].len() as i32{
+                pos[1] = nj as usize;
+            } else {
+                break;
+            }
+
+            // check character value at new pos
+            temp.push(characters[pos[0]][pos[1]]);
+            println!("{}", temp);
+
+            if temp == pattern{
+                sum += 1;
+                break;
+            }
+        }
+    }
+    return sum;
 }
