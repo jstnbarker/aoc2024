755a5412a70566d24f65f84bbdf8210da95480a3
Justin Barker <jstnbarker@pm.me>
2024-12-05 01:37:30 -0500

    Solve part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let mut line_iter = content.split("\n");
     
     // load ordering rules 
+    // O(n) runtime 
     let mut rules: Vec<[i32; 2]> = Vec::new();
     loop{
         let current = line_iter.next().unwrap();
@@ -22,6 +23,7 @@ fn main() {
     }
 
     // load updates
+    // O(nm) runtime
     let mut updates: Vec<Vec<i32>> = Vec::new();
     loop{
         let current = line_iter.next().unwrap();
@@ -35,9 +37,52 @@ fn main() {
                 if current.is_none(){
                     break;
                 }
-                update.push(current.unwrap().parse().expect("Did not parse to i32"))
+                update.push(current.unwrap().parse()
+                    .expect("Could not parse to i32"))
             }
             updates.push(update);
         }
     }
+
+    let mut sum = 0;
+    for update in updates{
+        if check_update(update.clone(), rules.clone()) {
+            sum += update[update.len()/2];
+            for value in update{
+                print!("{} ", value);
+            }
+            println!();
+        }
+    }
+    println!("{}", sum);
+}
+/*
+fn get_applicable_rules(update: Vec<i32>, rules: Vec<[i32;2]>) -> Vec<[i32; 2]>{
+    let mut to_apply: Vec<[i32;2]> = Vec::new();
+    for page in 0..update.len(){
+        for rule in rules
+    }
+    return to_apply;
+}
+*/
+
+
+fn check_update(update: Vec<i32>, rules: Vec<[i32; 2]>) -> bool {
+    for page in 0..update.len(){
+        for rule in &rules{
+            // only check applicable rule
+            if rule[0] == update[page]{
+                // then check remainder of list for rule[1]
+                for other_page in 0..update.len(){
+                    let temp = update[other_page]; 
+                    if rule[1] == temp {
+                        if other_page < page{
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return true;
 }
