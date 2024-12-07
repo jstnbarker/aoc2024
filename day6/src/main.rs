2f86ab00a6c3c61b0e3d49515a5ef1e99eb02d41
Justin Barker <jstnbarker@pm.me>
2024-12-07 18:01:58 -0500

    Make map an object

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,43 @@
 use std::fs;
 use std::env;
+use std::fmt;
 
+struct Map{
+    map: Vec<Vec<char>>,
+    limits: [usize; 2]
+}
+
+impl Map{
+    pub fn new(map: Vec<Vec<char>>) -> Self {
+        return Map{
+            limits: [map.len(), map[0].len()],
+            map
+        }
+    }
+
+    pub fn get(&self, coord: [usize;2])-> char{
+        return self.map[coord[0]][coord[1]].clone()
+    }
+    pub fn set(&mut self, coord: [usize;2], val: char){
+        self.map[coord[0]][coord[1]] = val;
+    }
+}
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut result: String = "".to_string();
+
+        for y in &self.map{
+            for x in y{
+                result = result + &x.to_string();
+            }
+            result+="\n";
+        }
+        return write!(f, "{} ", result)
+    }
+}
+
+#[derive(PartialEq)]
 struct Guard{
     pub pos: [usize;2],
     pub dir:  usize,
@@ -18,11 +55,20 @@ impl Guard{
         }
     }
 
+    pub fn clone(&self) -> Self {
+        Guard {
+            pos: self.pos.clone(),
+            axis_limit: self.axis_limit,
+            dir: self.dir.clone(),
+            movement_vector: self.movement_vector
+        }
+    }
+
     pub fn turn(&mut self, times: usize){
         self.dir = (self.dir + times) % 4
     }
 
-    pub fn step(&mut self) -> bool{
+    pub fn next(&mut self) -> [usize;2] {
         let ni = self.movement_vector[self.dir][0] + self.pos[0] as i32;
         let nj = self.movement_vector[self.dir][1] + self.pos[1] as i32;
 
@@ -31,13 +77,20 @@ impl Guard{
            nj < 0 || 
            nj >= self.axis_limit[1] as i32 
         {
+            return self.pos;
+        }
+        return [ni as usize, nj as usize];
+    }
+
+    pub fn step(&mut self) -> bool{
+        let temp = self.next();
+        if self.pos == temp{
             return false;
         }
-        self.pos = [ni as usize, nj as usize];
+        self.pos = temp;
         return true;
     }
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -58,23 +111,61 @@ fn main() {
         }
         map.push(content[line].chars().collect());
     }
-
-    let mut guard = Guard::new(guard_pos, [map.len(),map[0].len()]);
-    let crumbs = ['^','>','v','<'];
-    let mut unique_tiles = 1; // otherwise doesn't count start position
+    
+    let mut map: Map = Map::new(map);
+    let mut guard = Guard::new(guard_pos, map.limits);
+    let mut unique_tiles = 1; // otherwise doesn't count start positio  
+    let mut loops = 0;
     loop{
-        let tile = &mut map[guard.pos[0]][guard.pos[1]];
-        if *tile == '#'{
-            guard.turn(2);
+        if map.get(guard.next()) == '#'{ 
+            guard.turn(1)
+        } else {
+            let tile = map.get(guard.pos);
+            if tile == '.'{
+                map.set(guard.pos, 'x');
+                unique_tiles += 1;
+            } 
+            println!("{}", map);
+
+            let mut future = guard.clone();
+            if guard.next() != guard.pos{
+                let orig = map.get(guard.next()); // save next tile's value
+                map.set(guard.next(),'#');        // set next tile to '#'
+                println!("{}", map);              
+                future.turn(1);                   // turn ghost away from '#'
+                let max_steps = 10000;
+                for i in 0..max_steps{
+                    let future_tile = map.get(future.next());
+                    if future_tile == '#'{
+                        future.turn(1);
+                    } else if future == guard || i == max_steps-1 {
+                        loops += 1;
+                        break;
+                    }
+                    if !future.step(){
+                        break;
+                    }
+                }
+                map.set(guard.next(),orig);      // reset next tile to original value
+            } else {
+                break;
+            }
             guard.step();
-            guard.turn(3);
-        } else if *tile == '.'{
-            *tile = crumbs[guard.dir];
-            unique_tiles += 1;
         }
-        if !guard.step(){
-            break;
-        }
+
     }
-    println!("{}", unique_tiles);
+    println!("{}\n{}",unique_tiles,loops);
 }
+/*
+// 1614 too low
+// 1706 wrong
+// 1808 too high
+// 1853 too high
+// (untested) 1862 too high
+//
+// 10,000,000 1708
+//  1,000,000 1676
+//    100,000 1707
+//     10,000 1706
+//      1,000 2569
+*/
