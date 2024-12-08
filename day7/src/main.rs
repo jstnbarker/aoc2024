a14740fd8fa7cc06c82d799cf8ac1b09ba8dc64f
Justin Barker <jstnbarker@pm.me>
2024-12-07 23:16:36 -0500

    Load file

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+
+struct Expr{
+    result: u64,
+    terms: Vec<i32>
+}
+
+fn load(path: String) -> Vec<Expr> {
+    let content: String = fs::read_to_string(path)
+        .expect("File not found");
+    let content = content.split('\n');
+    let mut expressions: Vec<Expr> = Vec::new();
+    for line in content{
+        let (result, values) = line.split_once(':').unwrap();
+        let result: u64 = result.parse()
+            .expect("Result did not parse");
+        let mut terms: Vec<i32> = Vec::new();
+        for value in values.split(' '){
+            terms.push(value.parse()
+                .expect("Term did not parse"));
+        }
+        expressions.push(Expr{result, terms})
+    }
+    return expressions;
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let expr = load(args[1].clone());
 }
