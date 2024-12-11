49078a8edce2c31d3520d6115d86b91f10edd3d3
Justin Barker <jstnbarker@pm.me>
2024-12-10 20:08:19 -0500

    Decrement right_file index only when a swap does not occur. Otherwise causes right_file index to skip over some ids

--- day9p2/src/main.rs
+++ day9p2/src/main.rs
@@ -1,61 +1,95 @@
 use std::fs;
 use std::env;
-use std::collections::LinkedList;
 
 // -1 indicates free space
 // anything else indicates a file
-struct File {
-    id: i32,
+#[derive(Clone)]
+struct File { 
+    id: usize,
     len: usize,
+    free: usize,
 }
 
 fn load(path: String) -> String {
-    return fs::read_to_string(path).expect("no file");
+    fs::read_to_string(path).expect("no file").to_string()
 }
 
-fn expand(disk_map: String) -> LinkedList<File>{
-    let mut out: LinkedList<File> = LinkedList::new();
+fn expand(disk_map: String) -> Vec<File>{
+    let mut out: Vec<File> = Vec::new();
     let mut id = 0;
-
-    let chars: Vec<char> = disk_map.chars().collect();
-    for i in 0..chars.len()-2{
-        let block_count: usize = (chars[i] as i32 - 0x30) as usize;
-        let mut file_id = -1;
-        if i % 2 == 0{
-            file_id = id;
-            id+=1;
+    let mut char_iter = disk_map.chars();
+    loop{
+        match char_iter.next(){
+            Some(file_size) => {
+                let file_size: usize = (file_size as i32 - 0x30) as usize;
+                let mut free:usize = 0;
+                match char_iter.next(){
+                    Some(free_length) =>{
+                        free = (free_length as i32 - 0x30) as usize;
+                    }
+                    None => break
+                }
+                if free > usize::max_value()-2000{
+                    free = 0;
+                }
+                out.push(File{id, len: file_size, free});
+                id+=1;
+            }
+            None => break
         }
-        out.push_back(File{id:file_id, len:block_count});
-    }
-    return out;
+    } 
+    let last = out.len()-1;
+    out[last].free=0;
+    out
 }
 
-fn defrag(mut disk: LinkedList<File>)->LinkedList<File>{
-    let mut rptr = disk.back().iter().rev();
-    let mut lptr = disk.iter();
-    loop{
-        let lptr_file = lptr.next().unwrap();
-        if lptr_file.id == -1 {
+fn defrag(mut disk: Vec<File>)->Vec<File>{
+    let mut r = disk.len()-1;
+    while r > 0{
+        let mut swapped = false;
+        let mut l = 0;
+        while l < r {
+            if disk[l].free >= disk[r].len{
+                /* sum right's neighbor's free space and the current file's free space and size */
+                disk[r-1].free += disk[r].len+disk[r].free;
+                /* set right file's free to left's free minus right's size left file */
+                disk[r].free = disk[l].free - disk[r].len;
+                /* set left file's free space to zero */
+                disk[l].free = 0;
+
+                /* insert right file after left file */
+                let temp = disk.remove(r);
+                disk.insert(l+1, temp);
+                 
+                swapped = true;
+                break;
+            }
+            l+=1;
+        }
+        if !swapped {
+            r-=1;
         }
     }
+    disk
 }
 
 fn main() {
+    let max:usize = 10011500126037;
     let mut args = env::args();
     args.next();
     let disk = expand(load(args.next().unwrap()));
-    for file in disk {
-        println!("id: {:2}, len: {}", file.id, file.len);
-    }
-    /*
     let result = defrag(disk);
-    let mut sum = 0;
-    for val in result {
-        if val.id >= 0{
-            sum += val.id as usize * val.len
-
+    let mut next_file_start:usize = 0;
+    let mut sum: usize = 0;
+    for file in result {
+        //println!("id {:4}, size {:4}, free {:10}", file.id, file.len, file.free);
+        for i in next_file_start..next_file_start+file.len {
+            sum = sum + (file.id * i);
         }
+        next_file_start += file.len+file.free;
+    }
+    if sum >= max {
+        println!("Answer is too large");
     }
     println!("{}", sum);
-    */
 }
